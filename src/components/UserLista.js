f9f8f5266495e7fc6fd204091ab73c0fd4ea9b01
Paulo Victor <64162837+paulovictorralmeida@users.noreply.github.com>
2023-04-17 10:50:57 -0300

    Update UserLista.js

--- src/components/UserLista.js
+++ src/components/UserLista.js
@@ -6,33 +6,23 @@ function  UserLista() {
   const [listaUsers, setListaUsers] = useState([]);
 
   useEffect( () => {
-    api.get('usuarios').then(({data}) => {
+    api.get('/usuarios').then(({data}) => {
       setListaUsers(data)
     });
     console.log(listaUsers);
     // eslint-disable-next-line
-  }, ['']);
+  });
 
   return (
     <div className="UserLista">
-    <div>
-        <table>
-          <tr className=''>
-            {listaUsers.map(
-              user => <th key="{user.codigo}">{user.nomeCompleto}</th>
-            )};
-            <th>Nome social</th>
-            <th>Data de nascimento</th>
-            <th>Código</th>
-            <th>Sexo</th>
-            <th>E-mail</th>
-            <th>Estado</th>
-            <th>Município</th>
-            <th>Acessos</th>
-            <th>Situação</th>
-            <th>Data de vínculo</th>
-          </tr>
-        </table>
+      <div className="table__container">
+        {listaUsers.map(user => (
+          <li key={user.codigo}>
+            <b>Nome:</b>{user.nomeCompleto}<br/>
+            <b>Nome Social:</b>{user.nomeSocial}<br/>
+            <b>Email:</b>{user.email}<br/>
+          </li>
+        ))};
       </div>
     </div>
   );
