c94ba6f836e643f7373308a3e2e8d83f1837c496
Paulo Victor Rocha <paulowars@gmail.com>
2023-04-17 22:41:15 -0300

    Arrumada no CSS, Lupa arrumada no input, botao estilizado. Falta descolar botao da barra de pesquisa

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import  React from 'react';
 import './App.css';
 import api from './api';
-import UserLista from './components/UserLista';
-import Header from './components/Header';
 import SearchList from './components/SearchList'
 
 function App() {
 
     return (
     <div className="App">
-      <Header />
       <SearchList prop={api} />
-      <UserLista />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import UserLista from './components/UserLista';
+import Header from './components/Header';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
+    <UserLista />
   </React.StrictMode>
 );
 
