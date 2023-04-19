38c84b669fc4f116a57e6b7bf279c6ec55f744e4
Paulo Victor <64162837+paulovictorralmeida@users.noreply.github.com>
2023-04-19 14:30:06 -0300

    Update UserLista.js

--- src/components/UserLista.js
+++ src/components/UserLista.js
@@ -15,38 +15,40 @@ function  UserLista({valor}) {
 
   return (
     <div className="UserLista">
-      <table className="table__style">
-        <thead>
-          <tr>
-          <th>Código</th>
-          <th>Nome Completo</th>
-          <th>Nome Social</th>
-          <th>Data de Nascimento</th>
-          <th>Sexo</th>
-          <th>Email</th>
-          <th>Estado</th>
-          <th>Município</th>
-          <th>Situação</th>
-          <th>Número de acessos </th>
-          </tr>
-        </thead>
-        <tbody>
-          {Object.values(listaUsers).map(users => (
-            <tr key={users.codigo}>
-              <td>{users.codigo}</td>
-              <td>{users.nomeCompleto}</td>
-              <td>{users.nomeSocial}</td>
-              <td>{users.dataDeNascimento}</td>
-              <td>{users.sexo}</td>
-              <td>{users.email}</td>
-              <td>{users.estado}</td>
-              <td>{users.municipio}</td>
-              <td>{users.situacao}</td>
-              <td>{users.numeroDeAcessos}</td>
+      <div className="table__responsive">
+        <table className="table__style">
+          <thead>
+            <tr>
+            <th>Código</th>
+            <th>Nome Completo</th>
+            <th>Nome Social</th>
+            <th>Data de Nascimento</th>
+            <th>Sexo</th>
+            <th>Email</th>
+            <th>Estado</th>
+            <th>Município</th>
+            <th>Situação</th>
+            <th>Número de acessos </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {Object.values(listaUsers).map(users => (
+              <tr key={users.codigo}>
+                <td>{users.codigo}</td>
+                <td>{users.nomeCompleto}</td>
+                <td>{users.nomeSocial}</td>
+                <td>{users.dataDeNascimento}</td>
+                <td>{users.sexo}</td>
+                <td>{users.email}</td>
+                <td>{users.estado}</td>
+                <td>{users.municipio}</td>
+                <td>{users.situacao}</td>
+                <td>{users.numeroDeAcessos}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
